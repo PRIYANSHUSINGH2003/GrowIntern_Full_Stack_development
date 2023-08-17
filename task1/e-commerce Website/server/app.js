91edf954ad800e3f940f333eb342fb34c366bf55
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2023-08-17 18:50:47 +0530

    Update app.js

--- task1/e-commerce Website/server/app.js
+++ task1/e-commerce Website/server/app.js
@@ -1,1 +1,29 @@
+require("dotenv").config();
+const express = require("express")
+const app = express();
+const mongoose = require("mongoose");
+require("./db/connetion");
+const cookieParser = require("cookie-parser");
 
+
+
+
+const Products = require("./models/productsSchema");
+const defaultData = require("./defaultdata");
+const cors = require("cors");
+const router = require("./routes/router");
+
+
+app.use(express.json());
+app.use(cookieParser(""));
+app.use(cors());
+app.use(router);
+
+
+const port = 8005;
+
+app.listen(port,()=>{
+    console.log(`server is running on port number ${port}`);
+});
+
+defaultData();
